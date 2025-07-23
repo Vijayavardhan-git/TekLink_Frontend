2649e80390eabc77da18a1f8872b61af67458eaf
Vijayavardhan Pasagadugula <vijaya.vardhan@bertlabs.com>
2025-07-23 13:42:29 +0530

    refactor:Updated logo and site deatils

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
     <div className="navbar bg-base-300">
       <div className="flex-1">
         <Link to="/" className="btn btn-ghost text-xl">
-          👩‍💻 TekLink
+          👩‍💻 TekLync
         </Link>
       </div>
       {user && (
