0735e5ca0ef2ab85b1caeadd297a11a545c34fe2
vijayavardhan <vijayavardhan43@gmail.com>
2025-08-18 11:44:43 +0530

    Updated Premium page

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
+import { useEffect, useState } from "react";
 
 const Premium = () => {
+  const [isUserPremium, setIsUserPremium] = useState(false);
+  useEffect(() => {
+    verifyPremiumUser();
+  }, []);
+
+  const verifyPremiumUser = async () => {
+    const res = await axios.get(BASE_URL + "/premium/verify", {
+      withCredentials: true,
+    });
+
+    if (res.data.isPremium) {
+      setIsUserPremium(true);
+    }
+  };
   const handleBuyClick = async (type) => {
     const order = await axios.post(
       BASE_URL + "/payment/create",
@@ -29,39 +44,45 @@ const Premium = () => {
       theme: {
         color: "#F37254",
       },
-      // handler: verifyPremiumUser,
+      handler: verifyPremiumUser,
     };
 
     const rzp = new window.Razorpay(options);
     rzp.open();
   };
 
-  return (
+  return isUserPremium ? (
+    "You're are already a premium user"
+  ) : (
     <div className="m-10">
       <div className="flex w-full">
-        <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
+        <div className="card bg-base-300 rounded-box grid h-80 flex-grow place-items-center">
           <h1 className="font-bold text-3xl">Silver Membership</h1>
           <ul>
             <li> - Chat with other people</li>
-            <li> - 100 connection requests per day</li>
+            <li> - 100 connection Requests per day</li>
+            <li> - Blue Tick</li>
+            <li> - 3 months</li>
           </ul>
           <button
+            onClick={() => handleBuyClick("gold")}
             className="btn btn-secondary"
-            onClick={() => handleBuyClick("silver")}
           >
             Buy
           </button>
         </div>
         <div className="divider divider-horizontal">OR</div>
-        <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
+        <div className="card bg-base-300 rounded-box grid h-80 flex-grow place-items-center">
           <h1 className="font-bold text-3xl">Gold Membership</h1>
           <ul>
             <li> - Chat with other people</li>
-            <li> - Unlimited connections requests per day</li>
+            <li> - Inifinite connection Requests per day</li>
+            <li> - Blue Tick</li>
+            <li> - 6 months</li>
           </ul>
           <button
-            className="btn btn-primary"
             onClick={() => handleBuyClick("gold")}
+            className="btn btn-primary"
           >
             Buy
           </button>
